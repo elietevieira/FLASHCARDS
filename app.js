26ff859a38a1f6332c0793b26871888c98848bd8
elietevieira <129619588+elietevieira@users.noreply.github.com>
2025-06-04 20:53:25 -0300

    .

--- app.js
+++ app.js
@@ -14,6 +14,15 @@
     </div>
     </div>
     `
+
+    let respostaEstaVisivel = False 
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+
+    }
+
+
     container.appendChild(cartao)
 
 }
